d4ebba849aeafe1de8cbd7f1a591cd533cd7e717
jinahh <RhoJinAh@rhojinahui-macbook-pro.local>
2018-08-08 16:54:09 +0900

    Initial Commit

---
+++ test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by RhoJinAh on 2018. 8. 8..
+//  Copyright © 2018년 RhoJinAh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
